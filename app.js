df01c01d78a4458e6dc14259362e933f8aac2186
Mason-GR <mase210@gmail.com>
2021-07-12 13:23:50 -0400

    Completed exercises 1-3

--- app.js
+++ app.js
@@ -4,7 +4,18 @@ console.log(
 );
 
 // Exercise 1
+const firstName = "Mason";
+let lastName = "Gregory";
+var age = 30;
 
 // Exercise 2
+let fullName = firstName + " " + lastName;
+let templateFullName = `${firstName} ${lastName}`;
+console.log(fullName);
+console.log(templateFullName);
 
 // Exercise 3
+let city = "Ladson";
+let passTime = "collecting playing cards";
+let myStory = `\Hello! My name is ${fullName}. I live in ${city}. I enjoy ${passTime} and coding!`;
+console.log(myStory);
